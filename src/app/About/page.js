92db1dff679c62bb16ece6276abf1aebc8f3f555
Avaneesh <yourname@example.com>
2023-10-07 09:22:35 +0530

    My commit

---
+++ src/app/About/page.js
@@ -0,0 +1,13 @@
+import styles from "../page.module.css";
+
+export default function About() {
+    return (
+        <main className={styles.main}>
+        <img className={styles['img']}
+        src="/final resized pic.jpg"/>
+        <h1>About me</h1>
+        <p>I am Avaneesh Mahendra a engineering student of PES University. I am a python enthusiast. I also like to learn new things on engineering.</p>
+</main>
+    ) 
+    
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,31 @@
 import styles from "./page.module.css";
 
+
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Next.js + Tailwind CSS Boilerplate</h1>
+      <h1>Avaneesh Mahendra</h1>
+      <h3>Engineering Student</h3>
       <p>
-        This is a boilerplate for Next.js 13 with Tailwind CSS 2.2, PostCSS 8,
-        and CSS Modules.
+        Welcome
       </p>
+        <img className={styles['img']}
+        src="/final resized pic.jpg"/>
+        <div className={styles.links}>
+        <a href="https://github.com/avaneeshmahendra29" target="_blank">Github</a>
+        <a href="https://www.instagram.com/avaneeshmahendra29" target="_blank">Instagram</a>
+        <a href="https://www.linkedin.com/in/avaneesh-mahendra-315241294/" target="_blank">Linkedin</a>
+        </div>
+        <div className={styles.links}>
+        <a href="./About" target="_blank">About</a>
+        <a href="./Projects" target="_blank">Projects</a>
+        <a href="./Blog" target="_blank">Blog</a>
+
+        </div>
     </main>
-  );
+  )
+  {
+
+  }
 }
+  
